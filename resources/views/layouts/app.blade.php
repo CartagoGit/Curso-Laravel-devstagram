f8a70d5e2f497f0a3ee31ddb4de2090f02a3501d
Cartago <cartago.relaxingcup@gmail.com>
2023-06-21 20:48:48 +0200

    Comenzamos a crear lo que es devstagram

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,15 +11,26 @@
 
 </head>
 
-<body>
-    {{-- <nav>
-        <a href="/">Principal</a>
-        <a href="/nosotros">Nosotros</a>
-        <a href="/tienda">Tienda</a>
-    </nav> --}}
-    <h1 class="text-4xl font-extrabold">@yield('titulo')</h1>
-    <hr>
-    @yield('contenido')
+<body class="bg-gray-100">
+    <header class="p-5 border-b bg-white shadow">
+        <div class="conatiner mx-auto flex justify-between items-center">
+
+            <h1 class="text-3xl font-black">DevStagram</h1>
+
+            <nav>
+                <a class="font-bold uppercase text-gray-600 text-sm " href="#">Login</a>
+                <a class="font-bold uppercase text-gray-600 text-sm" href="#">Crear Cuenta</a>
+            </nav>
+        </div>
+    </header>
+
+    <main class="container mx-auto mt-10">
+        <h2 class="font-black text-center text-3xl mb-10">@yield('titulo')</h2>
+        @yield('contenido')
+    </main>
+    <footer>
+
+    </footer>
 
 </body>
 
--- resources/views/nosotros.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('titulo')
-    ¿Quiénes somos?
-@endsection()
-
-@section('contenido')
-    Sobre nosotros
-@endsection()
