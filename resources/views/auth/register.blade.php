3724d405f289faf570e6bf66e54126e4bb94f665
Cartago <cartago.relaxingcup@gmail.com>
2023-06-21 21:34:11 +0200

    Comenzamos a crear el formulario de registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,35 @@
 @extends('layouts.app')
 
 @section('titulo')
-Registrarse
+    Registrarse en DevStagram
 @endsection
 
 @section('contenido')
-Pagina pa registrarse
+    <div class="md:flex">
+        <div class="md:w-1/2">
+            <p>Imagen aquí</p>
+        </div>
+
+        <div class="md:w-1/2">
+            <form action="">
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Nombre
+                    </label>
+                    <input id="name" type="text" name="name" placeholder="Nombre"
+                        class="border p-3 w-full rounded-lg">
+
+                </div>
+
+                <div class="mb-5">
+                    <label for="nickname" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Nick del usuario
+                    </label>
+                    <input id="nickname" type="text" name="nickname" placeholder="Nick del usuario"
+                        class="border p-3 w-full rounded-lg">
+
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
