c29996a09466b4ecce54f3403c1042d1002ca51a
Cartago <cartago.relaxingcup@gmail.com>
2023-06-25 00:14:19 +0200

    Creamos la estrucutra de la pagina de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,49 @@
 @extends('layouts.app')
 
 @section('titulo')
-Login
+    Login
 @endsection
 
 @section('contenido')
-Pa loguearse
+    <div class="md:flex md:justify-center md:gap-10 md:items-center">
+        <div class="md:w-6/12 p-5">
+            <img src="{{ asset('img/auth/login.jpg') }}" alt="Imagen de registro de usuario" class="rounded-lg">
+        </div>
+
+        <div class="md:w-4/12 p-6 bg-white rounded-lg shadow-xl">
+            <form action="{{ route('login') }}" method="POST" novalidate>
+                @csrf
+
+                <div class="mb-5">
+                    <label for="nick" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Nick o email del usuario
+                    </label>
+                    <input id="nick" type="text" name="nick" placeholder="Nick del usuario"
+                        class="border p-3 w-full rounded-lg @error('nick') border-red-500 @enderror"
+                        value="{{ old('nick') }}" />
+                    @error('nick')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center w-full">{{ $message }}</p>
+                    @enderror
+                </div>
+
+
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Contraseña
+                    </label>
+                    <input id="password" type="password" name="password" placeholder="Contraseña"
+                        class="border p-3 w-full rounded-lg @error('password') border-red-500 @enderror"
+                        value="{{ old('password') }}" />
+                    @error('password')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center w-full">{{ $message }}</p>
+                    @enderror
+                </div>
+
+
+
+                <input type="submit" value="Iniciar sesión"
+                    class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase w-full font-bold p-3 text-white rounded-lg" />
+            </form>
+        </div>
+    </div>
 @endsection
--- resources/views/main/dashboard.blade.php
+++ resources/views/main/dashboard.blade.php
@@ -8,9 +8,7 @@
     <div class="flex justify-center">
         <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
             <div class="md:w-8/12 lg:w-6/12 px-5">
-                <p>
-                    Imagen aqui
-                </p>
+                <img src="{{ asset('img/user/usuario.svg') }}" alt="imagen del usuario" />
             </div>
             <div class="md:w-8/12 lg:w-6/12 px-5">
                 <p class="text-gray-700 text-2xl">{{ auth()->user()->name }}</p>
