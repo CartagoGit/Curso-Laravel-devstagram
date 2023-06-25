b738c5a79e6cab2eb6be44718b32139e7254ce8a
Cartago <cartago.relaxingcup@gmail.com>
2023-06-25 15:03:55 +0200

    Añadimos estilos responsive

--- resources/views/main/dashboard.blade.php
+++ resources/views/main/dashboard.blade.php
@@ -6,13 +6,31 @@
 
 @section('contenido')
     <div class="flex justify-center">
-        <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
-            <div class="md:w-8/12 lg:w-6/12 px-5">
+        <div class="w-full flex flex-col items-center sm:w-8/12 md:flex-row lg:w-6/12 ">
+            <div class="w-8/12 md:w-6/12 px-5">
                 <img src="{{ asset('img/user/usuario.svg') }}" alt="imagen del usuario" />
             </div>
-            <div class="md:w-8/12 lg:w-6/12 px-5">
+            <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col md:justify-center items-center py-10">
                 {{-- <p class="text-gray-700 text-2xl">{{ auth()->user()->name }}</p> --}}
                 <p class="text-gray-700 text-2xl">{{ $user->name }}</p>
+                <p class="text-gray-800 text-sm mb-3 font-bold">
+                    0
+                    <span class="font-normal">
+                        Seguidores
+                    </span>
+                </p>
+                <p class="text-gray-800 text-sm mb-3 font-bold">
+                    0
+                    <span class="font-normal">
+                        Siguiendo
+                    </span>
+                </p>
+                <p class="text-gray-800 text-sm mb-3 font-bold">
+                    0
+                    <span class="font-normal">
+                        Posts
+                    </span>
+                </p>
             </div>
         </div>
     </div>
