fabf48e322845546c2545b269ad3eec1bcd52bef
Cartago <cartago.relaxingcup@gmail.com>
2023-06-25 15:11:55 +0200

    Añadimos cambios en el perfil y en el responsive del dashboard

--- resources/views/main/dashboard.blade.php
+++ resources/views/main/dashboard.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 
 @section('titulo')
-    Tu Cuenta
+    Perfil de {{ $user->username }}
 @endsection
 
 @section('contenido')
     <div class="flex justify-center">
-        <div class="w-full flex flex-col items-center sm:w-8/12 md:flex-row lg:w-6/12 ">
-            <div class="w-8/12 md:w-6/12 px-5">
+        <div class="w-full flex flex-col items-center sm:w-8/12 md:flex-row xl:w-6/12 ">
+            <div class="w-6/12 px-5">
                 <img src="{{ asset('img/user/usuario.svg') }}" alt="imagen del usuario" />
             </div>
-            <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col md:justify-center items-center py-10">
+            <div class="md:w-8/12 xl:w-6/12 px-5 flex flex-col md:justify-center items-center md:items-start py-10">
                 {{-- <p class="text-gray-700 text-2xl">{{ auth()->user()->name }}</p> --}}
-                <p class="text-gray-700 text-2xl">{{ $user->name }}</p>
+                <p class="text-gray-700 text-2xl mb-5">{{ $user->name }}</p>
                 <p class="text-gray-800 text-sm mb-3 font-bold">
                     0
                     <span class="font-normal">
