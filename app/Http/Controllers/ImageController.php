29d75658e1b66822fa61ce0964285d6cb26bd523
Cartago <cartago.relaxingcup@gmail.com>
2023-06-28 18:05:53 +0200

    Creamos la subida de archivos al servidor y formateamos la data de la imagen subida

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 use Intervention\Image\Facades\Image;
 
 class ImageController extends Controller
@@ -13,12 +14,29 @@ class ImageController extends Controller
 	{
 		//  return "Imagen controller";
 		$image = $request->file('file');
-		// $nombreImage = Str::uuid() . '.' . $image->extension();
-		// $imagenServidor = Image::make($image);
-		// $imagenServidor->fit(1000,1000);
+		$nombreImage = Str::uuid() . '.' . $image->extension();
+		$imagenServidor = Image::make($image);
+		$imagenServidor->fit(1000, 1000);
 		// $imagenPath = public_path('uploads') . '/images/' . $nombreImage;
-		// $imagenServidor->save($imagenPath);
+		$rutaCarpeta = public_path('uploads/images');
+		if (!file_exists($rutaCarpeta)) {
+			mkdir($rutaCarpeta, 0755, true);
+		}
+		$imagenPath = $rutaCarpeta . '/' . $nombreImage;
+		$imagenServidor->save($imagenPath);
 		// $image->store('images', 'public');
-		return response()->json(['image' => $image->extension()]);
+		$imageData = [
+			'data' => $imagenServidor->filesize(),
+			'width' => $imagenServidor->width(),
+			'height' => $imagenServidor->height(),
+			'mime' => $imagenServidor->mime(),
+			'extension' => $image->extension(),
+			'original_name' => $image->getClientOriginalName(),
+			'name' => $nombreImage,
+			'path' => $rutaCarpeta,
+			'path_full' => $imagenPath,
+		];
+
+		return response()->json(['data' => $imageData]);
 	}
 }
