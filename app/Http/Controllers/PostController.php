b09d307458e54fcae77a7206e837e2fef53c10d6
Cartago <cartago.relaxingcup@gmail.com>
2023-06-29 22:09:55 +0200

    Creamos validaciones para crear la publicacion

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,36 +7,36 @@ use Illuminate\Foundation\Auth\User;
 
 class PostController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware(['auth']);
-    }
-    //
-    public function index(string $userPath)
-    {
-        if (!User::where('path', $userPath)->exists()) {
-            $actualUser = auth()->user();
-            return redirect('/' . $actualUser->path);
-        }
-        $user = User::where('path', $userPath)->first();
+	public function __construct()
+	{
+		$this->middleware(['auth']);
+	}
+	//
+	public function index(string $userPath)
+	{
+		if (!User::where('path', $userPath)->exists()) {
+			$actualUser = auth()->user();
+			return redirect('/' . $actualUser->path);
+		}
+		$user = User::where('path', $userPath)->first();
 
-        return view('main.dashboard', ['user' => $user]);
-    }
+		return view('main.dashboard', ['user' => $user]);
+	}
 
-    public function create(){
-        return view('posts.create');
-    }
+	public function create()
+	{
+		return view('posts.create');
+	}
 
-	 public function store(Request $request){
-		  $this->validate($request, [
-				'titulo' => 'required|max:255',
-				'descripcion' => 'required|max:2200',
-		  ]);
+	public function store(Request $request)
+	{
+		$this->validate($request, [
+			'titulo' => 'required|max:255',
+			'descripcion' => 'required|max:2200',
+		]);
 
-		  $request->user()->posts()->create([
-				'body' => $request->body
-		  ]);
 
-		  return back();
-	 }
+
+		return back();
+	}
 }
