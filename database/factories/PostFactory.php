7caeccd050788f67e59b8fc660decdd770a9ee84
Cartago <cartago.relaxingcup@gmail.com>
2023-06-29 21:54:02 +0200

    Cambiamos el tipo de faker a crear para descripcion limitando el numero de palabras

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -19,8 +19,12 @@ class PostFactory extends Factory
 		return [
 			//* -> Para cargar los alias de composer directamente para wsl
 			// composer dump-autoload
-			'title' => $this->faker->sentence(),
-			'description' => $this->faker->paragraph(),
+
+			//* -> Para ejecutar el factory
+			//* sail artisan tinker
+			// Post::factory()->count(100)->create();
+			'title' => $this->faker->sentence(5),
+			'description' => $this->faker->sentece(20),
 			'image' => $this->faker->uuid() . 'jpg',
 			'user_id' => $this->faker->randomElement([1, 2, 4, 5, 6, 7]),
 		];
