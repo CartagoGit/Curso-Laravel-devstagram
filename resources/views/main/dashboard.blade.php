ef1ad9769778bcc47d984b82bfb8f8e67650f897
Cartago <cartago.relaxingcup@gmail.com>
2023-07-10 18:13:37 +0200

    Añadimos efectos a las publicaciones y ellipsis al texto de la publicacion y el titulo de la publicacion

--- resources/views/main/dashboard.blade.php
+++ resources/views/main/dashboard.blade.php
@@ -39,57 +39,36 @@
             Publicaciones de {{ $user->username }}
         </h2>
         @if ($posts->count())
-            <div class="flex flex-wrap justify-center">
+            <div class="flex flex-wrap justify-center ">
                 @foreach ($posts as $post)
                     <div
                         class="border border-gray-700 rounded-xl flex flex-col overflow-hidden shadow bg-gray-700 mx-4 my-4
-								flex basis-5/6 sm:basis-2/3 md:basis-2/5 lg:basis-1/4 xl:basis-1/5
-								">
+								flex basis-5/6 sm:basis-2/3 md:basis-2/5 lg:basis-1/4 xl:basis-1/5 cursor-pointer transition-all duration-300 hover:ring hover:ring-gray-500">
 
-                        <span class="p-2 w-full text-center bg-gray-700 text-gray-100">{{ $post->title }}</span>
-                        <a href="" class="">
-                            <img src="{{ $post->image }}" alt="Imagen de la publicación {{ $post->title }}">
-                        </a>
-                        <span class="p-2 bg-white border-gray-700 border-t">{{ $post->description }}</span>
-                        <span class="text-right text-xs p-1 text-gray-400">{{ $post->created_at }}</span>
+                        <span class="p-2 w-full text-center bg-gray-700 text-gray-100">
+                            <p class="line-clamp-1" title="{{ $post->title }}">{{ $post->title }} </p>
+                        </span>
+
+                        <img src="{{ $post->image }}" alt="Imagen de la publicación {{ $post->title }}">
+
+                        <div class="p-2 bg-white border-gray-700 border-t h-full ">
+
+                            <p class="line-clamp-5 " title="{{ $post->description }}">{{ $post->description }}
+                            </p>
+                        </div>
+                        <span class="text-right text-xs p-1 text-gray-400 mt-auto">{{ $post->created_at }}</span>
                     </div>
                 @endforeach
+
 
             </div>
         @else
             <div class="flex justify-center">
                 <div class="p-4 border-4 border-dashed border-gray-400 rounded-xl bg-white shadow">
-                    <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
+                    <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay publicaciones</p>
                 </div>
             </div>
         @endif
     </section>
-    {{-- <section class="container mx-auto mt-10">
-        <h2 class="text-4xl text-center font-black my-10">
-            Publicaciones de {{ $user->username }}
-        </h2>
-        @if ($posts->count())
-            <div class="grid p-15 md:p-0 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-3/4 md:w-full mx-auto justify-center grid-flow-dense">
 
-                @foreach ($posts as $post)
-                    <div class="border border-gray-700 rounded-xl flex flex-col overflow-hidden shadow   bg-gray-700">
-                        <span class="p-2 w-full text-center bg-gray-700 text-gray-100">{{ $post->title }}</span>
-                        <a href="" class="">
-                            <img src="{{ $post->image }}" alt="Imagen de la publicación {{ $post->title }}">
-                        </a>
-                        <span class="p-2 bg-white border-gray-700 border-t">{{ $post->description }}</span>
-                        <span class="text-right text-xs p-1 text-gray-400">{{ $post->created_at }}</span>
-                    </div>
-                @endforeach
-            </div>
-        @else
-            <div class="flex justify-center">
-                <div class="p-4 border-4 border-dashed border-gray-400 rounded-xl bg-white shadow">
-
-                    <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
-                </div>
-
-            </div>
-        @endif
-    </section> --}}
 @endsection
