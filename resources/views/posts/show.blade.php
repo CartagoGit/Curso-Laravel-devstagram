9fda38acd5a8c081a3caa802c633a67fc6965abf
Cartago <cartago.relaxingcup@gmail.com>
2023-07-12 20:48:43 +0200

    habilitamos poder ver perfiles y publicaciones sin estar autenticado, pero evitamos que se puedan crear publicaciones en dicho caso

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -26,42 +26,52 @@
             </div>
         </div>
         <form class="w-4/5 sm:w-[45%] flex flex-col">
-            <div class="shadow p-5 w-full rounded-lg bg-gray-200 flex flex-col h-full">
-                <p class="text-xl font-bold text-center mb-4">
-                    Agregar comentario
-                </p>
-                <div class="h-full flex flex-col h-full">
-                    @csrf
-                    <div class="relative flex flex h-full">
-                        <textarea id="comment" type="text" name="comment" minlength="10" maxlength="2200"
-                            placeholder="Agregar comentario..." oninput="updateCommentLength(this)"
-                            class="border p-3 w-full rounded-lg focus:outline-gray-300 resize-y h-[200px] min-h-[100px] h-full @error('comment') border-red-500 @enderror"></textarea>
-                        <span class="absolute text-xs right-0 -bottom-4 text-gray-700">
-                            <span id="comment-length">
-                               0
-                            </span>/2200
-                        </span>
-                        <script>
-                            function updateCommentLength(textarea) {
-                                let commentLength = textarea.value.length;
-                                document.getElementById('comment-length').textContent = commentLength;
-                            }
-                        </script>
+            @csrf
+				@if (auth()->check())
+
+                <div class="shadow p-5 w-full rounded-lg bg-gray-200 flex flex-col h-full">
+                    <p class="text-xl font-bold text-center mb-4">
+                        Agregar comentario
+                    </p>
+                    <div class="h-full flex flex-col h-full">
+                        <div class="relative flex flex h-full">
+                            <textarea id="comment" type="text" name="comment" minlength="10" maxlength="2200"
+                                placeholder="Agregar comentario..." oninput="updateCommentLength(this)"
+                                class="border p-3 w-full rounded-lg focus:outline-gray-300 resize-y h-[200px] min-h-full h-full @error('comment') border-red-500 @enderror"></textarea>
+                            <span class="absolute text-xs right-0 -bottom-4 text-gray-700">
+                                <span id="comment-length">
+                                    0
+                                </span>/2200
+                            </span>
+                            <script>
+                                function updateCommentLength(textarea) {
+                                    let commentLength = textarea.value.length;
+                                    document.getElementById('comment-length').textContent = commentLength;
+                                }
+                            </script>
+                        </div>
+                        @error('comment')
+                            <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center w-full">{{ $message }}</p>
+                        @enderror
                     </div>
-                    @error('comment')
-                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center w-full">{{ $message }}</p>
-                    @enderror
-						</div>
-					</div>
-					<button type="submit"
-						 class="mt-4 bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase w-full font-bold p-3 text-white rounded-lg">
-						 Enviar comentario
-					</button>
-			</form>
-			<div class="shadow p-5 mb-5 rounded-lg bg-gray-800 text-white w-4/5 sm:w-[calc(90%+1rem)] sm:w-full">
-				 <p class="text-xl font-bold text-center mb-4">
-					  Comentarios
-				 </p>
-			</div>
+                </div>
+                <button type="submit"
+                    class="mt-4 bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase w-full font-bold p-3 text-white rounded-lg">
+                    Enviar comentario
+                </button>
+            @else
+				<div class="p-5 w-full  grid place-items-center h-full">
+					<p class="shadow rounded-lg bg-sky-800 text-xl font-bold text-center text-white border-4 border-dashed border-gray-800 p-8 mb-4">
+						 Solo usuarios autenticados pueden hacer comentarios
+					</p>
+				</div>
+
+				@endif
+        </form>
+        <div class="shadow p-5 mb-5 rounded-lg bg-gray-800 text-white w-4/5 sm:w-[calc(90%+1rem)] sm:w-full">
+            <p class="text-xl font-bold text-center mb-4">
+                Comentarios
+            </p>
+        </div>
     </div>
 @endsection
