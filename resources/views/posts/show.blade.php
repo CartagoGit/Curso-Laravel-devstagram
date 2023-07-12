be2ccb1210bd35acc68921a26d4eee25cc9b214c
Cartago <cartago.relaxingcup@gmail.com>
2023-07-12 19:58:31 +0200

    Comenzamos a crear el panel para agregar nuevos comentarios

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,11 +5,11 @@
 @endsection
 
 @section('contenido')
-    <div class="container mx-auto flex flex-wrap justify-center">
+    <div class="container mx-auto flex flex-wrap justify-center gap-4">
         <div class="w-4/5 sm:w-[45%] ">
             <div class="flex justify-center w-full">
-                <img class="rounded-xl border-4 border-dashed  border-gray-300 sm:max-h-[400px] shadow-lg" src="{{ $post->image }}"
-                    alt="Imagen de la publicación {{ $post->title }}">
+                <img class="rounded-xl border-4 border-dashed  border-gray-300 sm:max-h-[400px] shadow-lg"
+                    src="{{ $post->image }}" alt="Imagen de la publicación {{ $post->title }}">
             </div>
 
 
@@ -26,6 +26,38 @@
             </div>
         </div>
         <div class="w-4/5 sm:w-1/2">
+            <div class="shadow p-5 mb-5 rounded-lg bg-gray-300">
+                <p class="text-xl font-bold text-center mb-4">
+                    Agregar comentario
+                </p>
+                <form action="">
+                    @csrf
+                    <div class="relative">
+                        <textarea id="descripcion" type="text" name="descripcion" minlength="10" maxlength="2200"
+                            placeholder="Descripción de la publicación" oninput="updateDescripcionLength(this)"
+                            class="border p-3 w-full rounded-lg focus:outline-gray-300 resize-y h-[200px] min-h-[100px] max-h-[500px]  @error('descripcion') border-red-500 @enderror">{{ old('descripcion') }}</textarea>
+                        <span class="absolute text-xs right-0 -bottom-3 text-gray-700">
+                            <span id="descripcion-length">
+                                {{ strlen(old('descripcion')) }}
+                            </span>/2200
+                        </span>
+                        <script>
+                            function updateDescripcionLength(textarea) {
+                                let descripcionLength = textarea.value.length;
+                                document.getElementById('descripcion-length').textContent = descripcionLength;
+                            }
+                        </script>
+                    </div>
+                    @error('descripcion')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center w-full">{{ $message }}</p>
+                    @enderror
+                </form>
+            </div>
+				<div class="shadow p-5 mb-5 rounded-lg bg-gray-700 text-white">
+					<p class="text-xl font-bold text-center mb-4">
+						 Comentarios
+					</p>
+				</div>
         </div>
     </div>
 @endsection
