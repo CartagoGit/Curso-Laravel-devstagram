40851795760be71e1ca5cd39e3ab71eb7ca88254
Cartago <cartago.relaxingcup@gmail.com>
2023-07-12 18:59:09 +0200

    Creamos la ventana de la publicación y la estilamos

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,31 @@
 @extends('layouts.app')
 
 @section('titulo')
-{{ $post->title  }}
+    {{ $post->title }}
 @endsection
 
 @section('contenido')
-whow
+    <div class="container mx-auto flex">
+        <div class="md:w-1/2">
+            <div class="flex justify-center w-full">
+                <img class="rounded-xl border-4 border-dashed  border-gray-300 sm:max-h-[400px] shadow-lg" src="{{ $post->image }}"
+                    alt="Imagen de la publicación {{ $post->title }}">
+            </div>
+
+
+            <div>
+                <div class="flex justify-between">
+                    <span class="font-bold">{{ $post->user->username }}</span>
+                    <span class="text-right">0 Likes</span>
+                </div>
+                <p class="text-sm text-gray-500">{{ $post->created_at->diffForHumans() }}</p>
+                <p class="mt-5 rounded-xl border-2 border-gray-600 p-3 bg-white shadow-lg">
+                    {{ $post->description }}
+
+                </p>
+            </div>
+        </div>
+        <div class="md:w-1/2">
+        </div>
+    </div>
 @endsection
